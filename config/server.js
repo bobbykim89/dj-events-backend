6e44203c1f520d7c8675b13eb2fb7e9691c189ff
bobbykim89 <bobby.sihun.kim@gmail.com>
2021-09-12 22:26:53 -0700

    initial creation of strapi backend for ej-events

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e23aa9800a825fdcf799f4b10205e8db'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0292f844-4611-4082-b684-095b46e3b61a'
+};
